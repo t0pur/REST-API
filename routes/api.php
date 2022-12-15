cd3e9370c59b02719ef47729efc4d54429e71660
t0pur <115615407+t0pur@users.noreply.github.com>
2022-12-15 16:39:57 +0100

    commit 3 - routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('people', [PeopleController::class, 'index']);
+Route::get('people/{people}', [PeopleController::class, 'read']);
+Route::post('people', [PeopleController::class, 'create']);
+Route::put('people/{people}', [PeopleController::class, 'update']);
+Route::delete('people/{people}', [PeopleController::class, 'delete']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
